c8a22db6b88aef53977713093cb5d0b0cc7bc07f
mrealpe <mrealpe@fiec.espol.edu.ec>
2024-12-06 22:03:17 +0000

    actualziado

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My p")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's ](https://docs.streamlit.io/)."
 )
